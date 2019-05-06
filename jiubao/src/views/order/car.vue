8f182fc270ba1d45ab7f67ebb71af6ac41c6c917
yangyulie <282133634@qq.com>
2019-05-06 18:51:58 +0800

    '购物车功能完善'

--- jiubao/src/views/order/car.vue
+++ jiubao/src/views/order/car.vue
@@ -5,8 +5,8 @@
     </headed>
     <div class="wrap secreenHei">
         <ul class="list">
-            <li v-for="(item,index) in datas.rows" :key="index">
-                <label :class="{show:item.checked}" class="isChecked"><span><img src="@/assets/imgs/icon_15.png" alt=""></span><input type="checkbox" :value="item.Id" v-model="item.checked"></label>
+            <li v-for="(item,index) in list" :key="index">
+                <label :class="{show:item.checked}" class="isChecked" @click="checkedFn(index)"><span><img src="@/assets/imgs/icon_15.png" alt=""></span><input type="checkbox" :value="item.Id" v-model="item.checked"></label>
                 <img :src="item.picurls" alt="">
                 <dl class="proInfoBox">
                     <dt>
@@ -16,19 +16,20 @@
                     <dd>
                         <span>￥{{item.price}}</span>
                         <div>
-                            <span class="reduce" @click="reduceFn(item)"></span>
-                            <input type="number" v-model="item.number" @input="changeFn(item)">
-                            <span class="add" @click="addFn(item)"></span>
+                            <span class="reduce" @click="reduceFn(index)"></span>
+                            <input type="number" v-model="item.number" @input="changeFn(index)">
+                            <span class="add" @click="addFn(index)"></span>
                         </div>
                     </dd>
                 </dl>
             </li>
         </ul>
         <div class="total">
-          <label :class="{show:allChecked}" class="isChecked"><span><img src="@/assets/imgs/icon_15.png" alt=""></span><input type="checkbox" v-model="allChecked">{{allChecked?"取消全选":"全选"}}</label>
+          <label :class="{show:allChecked}" class="isChecked"><span><img src="@/assets/imgs/icon_15.png" alt=""></span><input type="checkbox" v-model="allChecked" @change="allCheckedFn">{{allChecked?"取消全选":"全选"}}</label>
           <div class="totalBox">
             总计：<span>{{total}}</span>
           </div>
+          <button>去结算</button>
         </div>
     </div>
     <foot :is_now="3"></foot>
@@ -51,8 +52,8 @@ export default {
     return {
       datas:{},
       submitData:[],
-      total:"",
-      allChecked:false
+      allChecked:false,
+      list:[]
     };
   },
   mounted() {
@@ -64,49 +65,52 @@ export default {
     // console.log(this.data)
   },
   watch:{
-      allChecked(){
-        for(let i=0;i<this.datas.rows.length;i++){
-          this.datas.rows[i].checked = this.allChecked
-        }
-      },
-      submitData(){
-        console.log(222,this.submitData)
-        this.total = this.submitData.reduce((item,index)=>{
-          console.log(333,item,index)
-          if(index.checked){
-            return index.number*index.price+item
-          }
-          
-        },0)
-    }
+    
   },
   methods: {
     init() {
-        let storage=window.localStorage;
-        this.token = storage.getItem("token");
-        this.getCarListFn()
+      let storage=window.localStorage;
+      this.token = storage.getItem("token");
+      this.getCarListFn()
     },
     delate(){},
-    changeFn(data){
-      data.checked = true;
+    allCheckedFn(){
+      let arr = this.list;
+      for(let i=0;i<arr.length;i++){
+        arr[i].checked = this.allChecked
+        this.$set(this.list, i, this.list[i]);
+      }
     },
-    addFn(data){
-      let num = data.number*1+1;
-      data.checked = true;
-      data.number = num
+    changeFn(index){
+      this.list[index].checked = true;
+      this.$set(this.list, index, this.list[index]);
     },
-    reduceFn(data){
-      let num = data.number*1-1;
+    checkedFn(index){
+      this.list[index].checked = !this.list[index].checked;
+      if(!this.list[index].checked){
+        this.allChecked=false
+      }
+      this.$set(this.list, index, this.list[index]);
+    },
+    addFn(index){
+      let num = this.list[index].number*1+1;
+      this.list[index].number = num;
+      this.list[index].checked = true;
+      this.$set(this.list, index, this.list[index]);
+    },
+    reduceFn(index){
+      let num = this.list[index].number*1-1;
       if(num<=0){
         return;
       }
-      data.checked = true;
-      data.number = num
+      this.list[index].number = num;
+      this.list[index].checked = true;
+      this.$set(this.list, index, this.list[index]);
     },
     getCarListFn(){
         Api.getCarList().then(res=>{
             this.datas = res;
-            console.log(111,res)
+            this.list = res.rows;
         })
     },
     
@@ -114,7 +118,22 @@ export default {
   },
   computed: {
     // ...mapState(['app','app2',"data"])
-    
+    total(){
+      if(this.list&&this.list.length>0){
+        return this.list.reduce((item,index)=>{
+          console.log(23,index.checked)
+          if(index.checked){
+          console.log(123,index.number*index.price,item)
+            return index.number*index.price+item
+          }else{
+            return item
+          }
+          
+        },0)
+      }else{
+        return 0;
+      }
+    }
   }
 };
 </script>
@@ -162,7 +181,7 @@ export default {
             >img{
               width: 132px; margin: 0 20px 0 0;
             }
-            
+            .isChecked{ width: auto;}
             
         }
         li:last-child{
@@ -171,9 +190,12 @@ export default {
     }
     .total{
         background-color: #fff; border-top: 1px solid #c1c1c1; height: 85px; display: flex; justify-content: space-between; align-items: center; padding-left: 20px;
+        button{
+          width: 195px; height: 86px; background-color: #2892fe; color: #fff; font-size: 24px; text-align: center; line-height: 86px; border: 0;
+        }
     }
     .isChecked{
-       position: relative; display: flex; justify-content: space-between; align-items: center; font-size: 24px; color: #6e6e6e;
+       position: relative; display: flex; justify-content: flex-start; align-items: center; font-size: 24px; color: #6e6e6e; width: 148px;
       span{
         width: 32px; height: 32px; border-radius: 50%; border: 1px solid #ccc; overflow: hidden; display: block; margin-right: 20px;
       }
@@ -188,9 +210,12 @@ export default {
       display: block;
     }
     .totalBox{
-      font-size: 18px; color: #6e6e6e; 
+      font-size: 18px; color: #6e6e6e; margin: 0 20px; flex: 1;
       span{
         color: #d81e06; font-size: 24px; font-weight: bold;
+      }
+      span:before{
+        content: "￥"
       }
     }
 }
